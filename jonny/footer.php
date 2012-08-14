b9dca6aa04fa6eb72b7f85dbb986f3517a2dce5f
Jonny <chusiang.lai@gmail.com>
2012-08-14 20:29:15 +0800

    modify format.

--- jonny/footer.php
+++ jonny/footer.php
@@ -1,10 +1,13 @@
 <?php
 echo "
-			<p>
-			<span class=Comment>凍仁翔 (Chu-Siang, Lai)</span> / 
-			<span class=Comment><a href=http://jonny.ubuntu-tw.net target=_blank>http://jonny.ubuntu-tw.net</a></span><br>
-			<span class=Comment><a href=https://github.com/chusaing/helloTux target=_blank>https://github.com/chusaing/helloTux</a></span>
-			</p>
+	<p>
+		<br>
+		<span class=Comment>凍仁翔 (Chu-Siang, Lai)</span> / 
+		<span class=Comment><a href=http://jonny.ubuntu-tw.net target=_blank>http://jonny.ubuntu-tw.net</a></span><br>
+		<span class=Comment><a href=https://github.com/chusaing/helloTux target=_blank>https://github.com/chusaing/helloTux</a></span>
+		<br>
+		<br>
+	</p>
 ";
 
 ?>
