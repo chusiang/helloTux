1c6eb0dbeaa6fc5c2fe5ab849b01c74b79e8f398
Jonny <chusiang.lai@gmail.com>
2012-08-15 18:01:30 +0800

    #22 切割/獨立 header 區塊 finish\!

---
+++ jonny/frame_header.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "
+	<ul class=navigation_menu>
+		<li><a href=# accesskey=U title=上方功能區塊>:::</a> </li>
+		<li><a href=http://hellotux.dev.drx.tw/index.php>回首頁</a></li>
+		<li><a href=#>網站導覽</a></li>
+	</ul>
+";
+?>
