e255510ab97958d286eea443912867414e5d2dd0
Jonny <chusiang.lai@gmail.com>
2012-08-15 17:43:55 +0800

    add a form for header to custom.php?id=xxx

--- jonny/frame_sidebar.php
+++ jonny/frame_sidebar.php
@@ -1,8 +1,25 @@
 <?php
+
+if (isset($_POST["custom_id"])) {
+	$custom_id = $_POST["custom_id"];
+	header("Location:custom.php?id=$custom_id");
+}
+
 echo "
 
 <a class='accesskey' href='#' accesskey='R' title='左方相關連結區塊'>:::</a><br/>
 <div class='menu_vertical'>
+
+	<form name=custom_switch method=post action=>
+		<input type=text name=custom_id id=custom_id size=12 value=jonny>
+		<input type=submit name=btnCustom id=btnCustom value=GO>
+	</form>
+
+</div>
+
+";
+
+/*
 	<ul>
 		<li><a href='#'>選單連結1</a></li>
 		<li><a href='#'>選單連結2</a></li>
@@ -11,7 +28,6 @@ echo "
 		<li><a href='#'>選單連結5</a></li>
 		<li><a href='#'>選單連結6</a></li>
 	</ul>
-</div>
+ */
 
-";
 ?>
