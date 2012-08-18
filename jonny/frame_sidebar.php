18985d5e2cb940f655ca04919314e4034f731362
Jonny <chusiang.lai@gmail.com>
2012-08-18 12:21:49 +0800

    default test user change to tux.

--- jonny/frame_sidebar.php
+++ jonny/frame_sidebar.php
@@ -11,8 +11,8 @@ echo "
 <div class='menu_vertical'>
 
 	<form name=custom_switch method=post action=>
-		<input type=text name=custom_id id=custom_id size=12 value=jonny>
-		<input type=submit name=btnCustom id=btnCustom value=GO>
+		<input type=submit name=btnCustom id=btnCustom value=hello>
+		<input type=text name=custom_id id=custom_id size=12 value=tux>
 	</form>
 
 	 <a href='http://whos.amung.us/show/um9r1aql'><img src='http://whos.amung.us/swidget/um9r1aql.gif' alt='Online counter' title='線上人數' border='0' width='80' height='15' /></a>
