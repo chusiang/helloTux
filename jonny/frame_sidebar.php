027d0401f632effcb0e82dccc6b10c43bd436dea
Jonny <chusiang.lai@gmail.com>
2012-08-21 10:47:45 +0800

    fix format.

--- jonny/frame_sidebar.php
+++ jonny/frame_sidebar.php
@@ -6,37 +6,32 @@ if (isset($_POST["custom_id"])) {
 }
 
 echo "
+	<br>
 
-<a class='accesskey' href='#' accesskey='R' title='左方相關連結區塊'>:::</a><br/>
-<div class='menu_vertical'>
-	
-<label>
-	<font size=2 color=#ffffff>
-	前往自訂頁面
-	</font>
-</label>
+	<div class=breadcrumbs>
+				<a class='accesskey' href='#' accesskey='R' title='左方相關連結區塊'>:::</a>
+		<font size=2 color=#7f7f7f>側邊欄</font> <br>
+	</div>
 
-	<form name=custom_switch method=post action=>
-		<input type=submit name=btnCustom id=btnCustom value=hello>
-		<input type=text name=custom_id id=custom_id size=12 value=tux>
-	</form>
+	<div class=paragraph>
 
-	 <a href='http://whos.amung.us/show/um9r1aql'><img src='http://whos.amung.us/swidget/um9r1aql.gif' alt='Online counter' title='線上人數' border='0' width='80' height='15' /></a>
+		<p>
+			<ul>
+				<li>
+					<font size=2 color=#ffffff>前往自訂頁面</font>
+				
+					<form name=custom_switch method=post action=>
+						<input type=submit name=btnCustom id=btnCustom value=hello>
+						<input type=text name=custom_id id=custom_id size=7 value=tux>
+					</form>
+					
+				</li>
+			</ul>
+		</p>
 
-</div>
+		<p>
+			<a href='http://whos.amung.us/show/um9r1aql'><img src='http://whos.amung.us/swidget/um9r1aql.gif' alt='Online counter' title='線上人數' border='0' width='80' height='15' /></a>
+		</p>
+	</div>
 
 ";
-
-/*
-
-	<ul>
-		<li><a href='#'>選單連結1</a></li>
-		<li><a href='#'>選單連結2</a></li>
-		<li><a href='#'>選單連結3</a></li>
-		<li><a href='#'>選單連結4</a></li>
-		<li><a href='#'>選單連結5</a></li>
-		<li><a href='#'>選單連結6</a></li>
-	</ul>
- */
-
-?>
