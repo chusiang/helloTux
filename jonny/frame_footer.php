2f6ee2cb5bd7d352f59f7107f43f33283ab7269a
chusiang <chusiang.lai@gmail.com>
2013-01-03 23:02:43 +0800

    change github link.

--- jonny/frame_footer.php
+++ jonny/frame_footer.php
@@ -4,7 +4,7 @@ echo "
 		<br>
 		<span class=Comment>凍仁翔 (Chu-Siang, Lai)</span> / 
 		<span class=Comment><a href=http://note.drx.tw target=_blank>http://note.drx.tw</a></span><br>
-		<span class=Comment><a href=https://github.com/chusaing/helloTux target=_blank>https://github.com/chusaing/helloTux</a></span>
+		<span class=Comment><a href=https://github.com/chusiang/helloTux target=_blank>https://github.com/chusiang/helloTux</a></span>
 		<br>
 		<br>
 	</p>
