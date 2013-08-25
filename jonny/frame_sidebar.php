7bdeddf0a671be8f1d9d83bd9fccce30585a5ca7
chusiang <chusiang.lai@gmail.com>
2013-08-26 07:05:43 +0800

    use 'div#breadcrumbs' and 'section#paragraph' css style.

--- jonny/frame_sidebar.php
+++ jonny/frame_sidebar.php
@@ -1,37 +1,36 @@
 <?php
 
-if (isset($_POST["custom_id"])) {
-	$custom_id = $_POST["custom_id"];
+if (isset($_POST['custom_id'])) {
+	$custom_id = $_POST['custom_id'];
 	header("Location:http://" . $_SERVER['HTTP_HOST'] . "/custom.php?id=$custom_id");
 }
 
 echo "
-	<br>
+	<br />
 
-	<div class=breadcrumbs>
-				<a class='accesskey' href='#' accesskey='R' title='左方相關連結區塊'>:::</a>
-		<font size=2 color=#7f7f7f>側邊欄</font> <br>
+	<div id='breadcrumbs'>
+		<a class='accesskey' href='#' accesskey='R' title='左方相關連結區塊'>:::</a> 側邊欄 <br />
 	</div>
 
-	<div class=paragraph>
+	<section id='paragraph'>
 
 		<p>
 			<ul>
 				<li>
-					<font size=2 color=#ffffff>前往自訂頁面</font>
+					前往自訂頁面 <br />
 				
-					<form name=custom_switch method=post action=>
-						<input type=submit name=btnCustom id=btnCustom value=hello>
-						<input type=text name=custom_id id=custom_id size=7 value=tux>
+					<form name='custom_switch' method='post' action=''>
+						<input type='submit' name='btnCustom' id='btnCustom' value='hello'>
+						<input type='text' name='custom_id' id='custom_id' size='5' value='tux'>
 					</form>
 					
+				</li>
+				<li>
+					Wedgits <br />
+					<a href='http://whos.amung.us/show/um9r1aql'><img src='http://whos.amung.us/swidget/um9r1aql.gif' alt='Online counter' title='線上人數' border='0' width='80' height='15' /></a>
 				</li>
 			</ul>
 		</p>
 
-		<p>
-			<a href='http://whos.amung.us/show/um9r1aql'><img src='http://whos.amung.us/swidget/um9r1aql.gif' alt='Online counter' title='線上人數' border='0' width='80' height='15' /></a>
-		</p>
-	</div>
-
+	</section>
 ";
