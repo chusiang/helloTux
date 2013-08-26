d94a56b13735ebebc5219f75bd765669a9a87929
chusiang <chusiang.lai@gmail.com>
2013-08-26 23:01:21 +0800

    upgrade to HTML 5.

--- jonny/admin/view.php
+++ jonny/admin/view.php
@@ -45,8 +45,8 @@ function fnLoad($lang, $sql_record){
 	$btnDel = " 刪除 ";
 	$aryRid = array();
 
-	echo "<form name=del_record method=post action=>";
-	echo "<table class=table_dark>";
+	echo "<form name='del_record' method='post' action=''>";
+	echo "<table class='dark'>";
 
 	switch ($lang) {
 
@@ -56,7 +56,7 @@ function fnLoad($lang, $sql_record){
 		$btnModify = " 修改 ";
 		$btnDel = " 刪除 ";
 
-		echo "<tr><th><input type=checkbox name=chkClick_all id=chkClick_all></th> <th>套件</th> <th>敘述</th> <th colspan=2>備註</th></tr>";
+		echo "<tr><th><input type='checkbox' name='chkClick_all' id='chkClick_all'></th> <th>套件</th> <th>敘述</th> <th colspan='2'>備註</th></tr>";
 
 		$i = 0;
 
@@ -66,11 +66,11 @@ function fnLoad($lang, $sql_record){
 			$aryRid[$i] = $rid;
 
 			echo "<tr>
-				<td align=center><input name='chkbox[]' type='checkbox' value=$rid></td>
-				<td><a href=apt://$pkg>$name</a></td>
+				<td align='center'><input name='chkbox[]' type='checkbox' value=$rid></td>
+				<td><a href='apt://$pkg'>$name</a></td>
 				<td>$desc_tw</td>
 				<td>$comment</td>
-				<td><input type=button name=btnModify id=btnModify value=$btnModify onClick=location.href='modify.php?rid=$rid&rkey=$rkey'; /></td>
+				<td><input type='button' name='btnModify' id='btnModify' value='$btnModify' onClick=location.href='modify.php?rid=$rid&rkey=$rkey'; /></td>
 
 				</tr>";
 
@@ -87,7 +87,7 @@ function fnLoad($lang, $sql_record){
 		$btnModify = " Modify ";
 		$btnDel = " Delete ";
 
-		echo "<tr><th><input type=checkbox name=chkClick_all id=chkClick_all></th> <th>Package</th> <th>Description</th> <th colspan=2>comment</th></tr>";
+		echo "<tr><th><input type='checkbox' name='chkClick_all' id='chkClick_all'></th> <th>Package</th> <th>Description</th> <th colspan=2>comment</th></tr>";
 
 		$i = 0;
 
@@ -97,11 +97,11 @@ function fnLoad($lang, $sql_record){
 			$aryRid[$i] = $rid;
 
 			echo "<tr>
-				<td align=center><input name='chkbox[]' type='checkbox' value=$rid></td>
-				<td><a href=apt://$pkg>$name</a></td>
+				<td align='center'><input name='chkbox[]' type='checkbox' value=$rid></td>
+				<td><a href='apt://$pkg'>$name</a></td>
 				<td>$desc_en</td>
 				<td>$comment</td>
-				<td><input type=button name=btnModify id=btnModify value=$btnModify onClick=location.href='modify.php?rid=$rid&rkey=$rkey'; /></td>
+				<td><input type='button' name='btnModify' id='btnModify' value='$btnModify' onClick=location.href='modify.php?rid=$rid&rkey=$rkey'; /></td>
 				</tr>";
 
 			$i ++;
@@ -117,7 +117,9 @@ function fnLoad($lang, $sql_record){
 	echo "</table>";
 
 	echo "<br>
-		<center><input type=submit name=btnDel id=btnDel value=$btnDel></center>
+			<div align='center'>
+				<input type='submit' name='btnDel' id='btnDel' value='$btnDel'>
+			<?/div>
 		</form>";
 
 	//<span class=Comment> ---- </span><br>
@@ -144,16 +146,16 @@ if(isset($_POST['chkbox'])) {
 ?>
 
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE HTML>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-tw">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="keyword" content="ubuntu, apt, apturl"/>
-<meta name="author" content="Developer: 凍仁翔 - jonny (at) drx.tw; Desgin: Violet - violet (at) drx.tw"/>
-<link type="text/css" href="../include/violet.css" rel="stylesheet">
-<script type="text/javascript" src="../include/jquery-1.7.1.min.js"></script>
-<script type="text/javascript" src="../include/selectd-to-install-ubuntu.js"></script>
-<title>helloTux</title>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="keyword" content="ubuntu, apt, apturl"/>
+	<meta name="author" content="Developer: 凍仁翔 - jonny (at) drx.tw; Desgin: Violet - violet (at) drx.tw"/>
+	<link type="text/css" href="../include/violet.css" rel="stylesheet">
+	<script type="text/javascript" src="../include/jquery-1.7.1.min.js"></script>
+	<script type="text/javascript" src="../include/selectd-to-install-ubuntu.js"></script>
+	<title>helloTux</title>
 </head>
 
 <body>
@@ -161,52 +163,50 @@ if(isset($_POST['chkbox'])) {
 <div id="container">
 
 	<!--頁首-->
-	<div id="header">
-		<div id="header_menu">
+	<header>
+		<nav id="top">
 <?php
 include '../frame_header.php';
 ?>
-		</div>
-	</div>
+		</nav>
+	</header>
 
 	<!--外框架圍繞內容-->
 	<div id="wrapper">
 
-		<div id="menu_main">
-			<div class="menu_level">
-				<ul>
-					<li><?php echo "<a href=http://" . $_SERVER['HTTP_HOST'] . "/index.php>Home</a>"; ?></li>
-					<li><?php echo "<a href=http://" . $_SERVER['HTTP_HOST'] . "/pkg.php>Package</a>"; ?></li>
+		<nav id="menu_h">
+			<ul>
+				<li><?php echo "<a href=http://" . $_SERVER['HTTP_HOST'] . "/index.php>Home</a>"; ?></li>
+				<li><?php echo "<a href=http://" . $_SERVER['HTTP_HOST'] . "/pkg.php>Package</a>"; ?></li>
 <?php
 //if ($level == 0) {
 //	echo "<li><a href=http://" . $_SERVER['HTTP_HOST'] . "/admin/adm.php>Admin</a></li>";
 //}
 ?>
-					<li class="selected">View</li>
-					<li><?php echo "<a href=http://" . $_SERVER['HTTP_HOST'] . "/admin/add.php>Add</a>"; ?></li>
-				</ul>
-			</div>
-		</div>
+				<li id="selected">View</li>
+				<li><?php echo "<a href=http://" . $_SERVER['HTTP_HOST'] . "/admin/add.php>Add</a>"; ?></li>
+			</ul>
+		</nav>
 
 		<!--側邊欄-->
-		<div id="sidebar">
+		<aside>
 <?php
 include '../frame_sidebar.php';
 ?>
-		</div>
+		</aside>
 
 		<!--內容-->
 		<div id="content">
 
 			<!--麵包屑-->
-			<div class="breadcrumbs">
-				<a class="accesskey" href="#" accesskey="C" title="中央內容區塊">:::</a> 現在位置：<?php echo "<a href=http://" . $_SERVER['HTTP_HOST'] . "/index.php>首頁</a>"; ?> / 套件管理 <br/><br/>
+			<div id="breadcrumbs">
+				<a class="accesskey" href="#" accesskey="C" title="中央內容區塊">:::</a> 現在位置：<?php echo "<a href=http://" . $_SERVER['HTTP_HOST'] . "/index.php>首頁</a>"; ?> / 個人套件管理 <br/><br/>
 			</div>
 
 			<!--段落-->
-			<div class="paragraph">
+			<section id="paragraph">
 
-				<h1>View of Admin</h1>
+				<h1>Private Package List Manage</h1>
 
 				<p>
 				<div class="lang">
@@ -236,14 +236,14 @@ mysql_close($connection);
 
 ?>
 				</p>
-			</div>
+			</section>
 		</div>
 
-		<div id="footer">
+		<footer>
 <?php
 include '../frame_footer.php';
 ?>
-		</div>
+		</footer>
 	</div>
 </div>
 
