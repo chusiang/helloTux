240279ac35139b53a9abd139c9d650c54386920c
chusiang <chusiang.lai@gmail.com>
2013-08-26 23:02:11 +0800

    簡介內容補完。

--- jonny/index.php
+++ jonny/index.php
@@ -57,20 +57,21 @@ include 'frame_sidebar.php';
 
 				<p>
 				<ul>
-					<li>此專案使用 <a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GPL v2 or later</a> 授權，其主要目的是讓使用者直接<b>使用瀏覽器安裝 Linux 套件</b>，並<b>擁有專屬套件列表</b>，換句話說這會是個 Linux 版的 <a href="http://gfx.tw/" target="_blank">抓火狐</a>，現階段將以 Ubuntu 的 AptURL 為主，後期會試著將 OpenSUSE 的 One-Click 整合進來。</li>
+					<li><mark>helloTux</mark> 是一個提供管理並分享套件列表的網站。其一詞來自每個學程式的人都會寫到「Hello World」，而接觸 Linux 的人都知道其吉祥物的名稱是 Tux，於是「Hello Tux」就這樣誕生了！ <small>[1]</small></li>
 
-					<li><mark>helloTux</mark> 一詞來自每個學程式的人都會寫到「Hello World」，而接觸 Linux 的人都知道其吉祥物的名稱是 Tux，於是「Hello Tux」就這樣誕生了！ [註1]</li>
-
-					<li>其中文名稱尚未定案，目前有「企鵝您好」、「塔克斯您好」...</li>
+					<li>其中文名稱尚未定案，目前有「企鵝您好」、「塔克斯您好」... 等。</li>
 
 					<li>舊版網址為 <a href="old/ubuntu-11.10.html" target="_blank">old/ubuntu-11.10.html</a>。</li>
 
-					<li>目前使用到的技術有 Linux, Apache, Nginx, MySQL, PHP, jQuery, CSS。</li>
+					<li>目前使用到的技術有 Linux, Apache, Nginx, MySQL, PHP, jQuery, CSS, HTML5。</li>
+
+					<li>此專案使用 <a href="http://www.gnu.org/licenses/gpl.html" target="_blank">GPL v2 or later</a> 授權，其主要目的是讓使用者直接<b>使用瀏覽器安裝 Linux 套件</b>，並<b>擁有專屬套件列表</b>，換句話說這會是個 Linux 版的 <a href="http://gfx.tw/" target="_blank">抓火狐</a>，現階段將以 Ubuntu 的 AptURL 為主，後期會試著將 OpenSUSE 的 One-Click 整合進來。</li>
+
 				</ul>
 				</p>
 
 				<p>
-					<small><span class="Comment">[註] 我們改變世界了！ Hello World → Hello Tux。</span></small>
+					<small><span class="Comment">[1] 我們改變世界了！ Hello World → Hello Tux。</span></small>
 				</p>
 
 			</section>
