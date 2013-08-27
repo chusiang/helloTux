94c967669bb5d8fe1c222e091c5e3aa1c5d11889
chusiang <chusiang.lai@gmail.com>
2013-08-27 20:46:17 +0800

    於側欄麵包屑新增登錄/登出功能。

--- jonny/frame_sidebar.php
+++ jonny/frame_sidebar.php
@@ -5,32 +5,44 @@ if (isset($_POST['custom_id'])) {
 	header("Location:http://" . $_SERVER['HTTP_HOST'] . "/custom.php?id=$custom_id");
 }
 
-echo "
-	<br />
+?>
 
-	<div id='breadcrumbs'>
-		<a class='accesskey' href='#' accesskey='R' title='左方相關連結區塊'>:::</a> 側邊欄 <br />
-	</div>
+<br />
 
-	<section id='paragraph'>
+<div id='breadcrumbs'>
+	<a class='accesskey' href='#' accesskey='R' title='左方相關連結區塊'>:::</a> 
 
-		<p>
-			<ul>
-				<li>
-					前往自訂頁面 <br />
+<?php
+
+# - 取得使用者 ID 且不為空時。
+if (isset($_SESSION["ID"]) && $_SESSION["ID"] !="") {
+	$ID=$_SESSION["ID"];
+	echo "Hi, $ID <a href=https://" .$_SERVER['HTTP_HOST'] . "/login.php>[登出]</a>";
+} else {
+	echo "Hi, Guest <a href=https://" .$_SERVER['HTTP_HOST'] . "/login.php>[登錄]</a>";
+}
+
+?>
+</div>
+
+<section id='paragraph'>
+
+	<p>
+		<ul>
+			<li>
+				前往自訂頁面 <br />
+			
+				<form name='custom_switch' method='post' action=''>
+					<input type='text' name='custom_id' id='custom_id' size='5' value='tux'>
+					<input type='submit' name='btnCustom' id='btnCustom' value='hello'>
+				</form>
 				
-					<form name='custom_switch' method='post' action=''>
-						<input type='submit' name='btnCustom' id='btnCustom' value='hello'>
-						<input type='text' name='custom_id' id='custom_id' size='5' value='tux'>
-					</form>
-					
-				</li>
-				<li>
-					Wedgits <br />
-					<a href='http://whos.amung.us/show/um9r1aql'><img src='http://whos.amung.us/swidget/um9r1aql.gif' alt='Online counter' title='線上人數' border='0' width='80' height='15' /></a>
-				</li>
-			</ul>
-		</p>
+			</li>
+			<li>
+				Wedgits <br />
+				<a href='http://whos.amung.us/show/um9r1aql'><img src='http://whos.amung.us/swidget/um9r1aql.gif' alt='Online counter' title='線上人數' border='0' width='80' height='15' /></a>
+			</li>
+		</ul>
+	</p>
 
-	</section>
-";
+</section>
